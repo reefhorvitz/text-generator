378bab154cd370680d3850026f6ad4c5259ee649
reef <reef@beam.travel>
2022-12-15 22:31:37 +0200

    updated

--- text_server/urls.py
+++ text_server/urls.py
@@ -21,7 +21,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Stork API",
+        title="Text Server API",
         default_version='v1',
     ),
     public=True,
